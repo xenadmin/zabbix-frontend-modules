82046ec6d7c269e4d29dcb3aa852751d1223076d
xenadmin <xenadmin@meinekleinefarm.net>
2020-11-10 10:37:25 +0100

    Problems -> problems

--- add-latest_problems-to-monitoring-menu/Module.php
+++ add-latest_problems-to-monitoring-menu/Module.php
@@ -18,7 +18,7 @@ class Module extends \Core\CModule {
 		APP::Component()->get('menu.main')
 			->findOrAdd(_('Monitoring'))
 				->getSubmenu()
-					->insertAfter(_('Problems'),((new \CMenuItem(_('Latest Problems')))
+					->insertAfter(_('Problems'),((new \CMenuItem(_('Latest problems')))
 							->setUrl(new \CUrl('zabbix.php?action=problem.view&filter_show=3&filter_application=&filter_name=&filter_severities[2]=2&filter_severities[4]=4&filter_severities[3]=3&filter_severities[5]=5&filter_inventory[0][field]=type&filter_inventory[0][value]=&filter_evaltype=0&filter_tags[0][tag]=&filter_tags[0][operator]=0&filter_tags[0][value]=&filter_show_tags=0&filter_show_opdata=1&filter_unacknowledged=1&filter_show_timeline=1&filter_set=1'), 'zabbix.php'))
 					);
 	}
